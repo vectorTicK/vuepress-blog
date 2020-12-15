0c535b422f4111a3835f473c4c2670c5d57c8cc5
tick <tk1011@qq.com>
2020-12-15 09:08:43 +0800

    。。

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  base: '/vuepress-blog/',
+    title: '二羊话叹', // Title for the site. This will be displayed in the navbar.
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+      // Please keep looking down to see the available options.
+    }
+  }
---
+++ blog/_posts/2020-12-13-hello vuepress blog.md
@@ -0,0 +1,13 @@
+---
+title: Vuepress blog使用入门
+date: 2020-12-14
+tags: 
+  - BLOG
+  - VuePress
+author: 二羊话叹
+location: Lincang  
+---
+
+> 使用vuepress快速搭建博客，并部署在GitHub
+
+My content.
